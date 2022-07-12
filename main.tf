bb3468beaf91274f58bad8ad012c9a3bfc81a61a
akash katoch <87859777+akashkatoch@users.noreply.github.com>
2022-07-12 18:03:14 +0530

    update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ terraform {
 	}
 }
 
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIAYFV75SZQZ5FR5UVM"
+    secret_key =  "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"
+}
+
 resource "aws_instance"
 "aws_testing" {
 	ami = ami-0cff7528ff583bf9a
