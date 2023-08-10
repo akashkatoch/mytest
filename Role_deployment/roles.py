fa64857bf381c1cb3cbdfd71f923279dbfd86c9e
akash katoch <87859777+akashkatoch@users.noreply.github.com>
2023-08-10 10:43:37 +0530

    Update roles.py

--- Role_deployment/roles.py
+++ Role_deployment/roles.py
@@ -5,6 +5,7 @@ import json
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+
 def add_user_in_tenant(header):
     urllib3.disable_warnings()
     f = open('role.txt')
@@ -13,11 +14,10 @@ def add_user_in_tenant(header):
         for tenants in data["tenant"]:
             for new_account in data["account"]:
                 for new_member in data["members"]:
-                    logging.info(f'Gathering information to add {new_member} in account {new_account} under tenant {tenants}')
+                    logging.info(
+                        f'Gathering information to add {new_member} in account {new_account} under tenant {tenants}')
                     request_url = f'https://avm.mckinsey.com/role/aws.k8savm.io/{tenants}/{new_account}.{data["role"]}/group/access/member/add/{new_member}'
-                    response = requests.post(request_url, headers=header, verify=False)
-
-
+                    requests.post(request_url, headers=header, verify=False)
                     logging.info(f'user {new_member} added in {tenants}/{new_account}')
     except Exception as e:
         logging.info(f'Error received: {e}')
@@ -32,5 +32,3 @@ if __name__ == '__main__':
     }
 
     add_user_in_tenant(header)
-
-
